5e9d7e316a2a26a69627309d1615e2d693a4c218
kirti <avaiyakirtib@gmail.com>
2023-11-17 16:41:02 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const { config } = require("dotenv");
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+config();
+const PORT = process.env.PORT || 3000;
+const server = express();
+
+server.set("view engine", "ejs");
+server.use(express.static("public"));
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  return res.render("home");
+});
+
+server.get("/search/", async (req, res) => {
+  try {
+    const search = req.query?.search;
+    let response = await axios.get(
+      `https://savvytime.com/api/search/timezone?query=${search}`
+    );
+    // return res.json({
+    //   message: 'Success',
+    //   data: response.data
+    // })
+    return res.send(response.data);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      message: "Fail",
+    });
+  }
+});
+
+server.get("/time", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://timeapi.io/api/Time/current/coordinate/?latitude=${req.query?.lat}&longitude=${req.query?.lon}`
+    );
+    return res.status(200).json({
+      status: "Success",
+      data: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      message: "Fail",
+    });
+  }
+  // return res.render('home')
+});
+
+server.post("/time/convert", async (req, res) => {
+  try {
+    console.log(req.body);
+    const date = req.body.date;
+    console.log(date.split("T"));
+    const dateOnly = date.split("T");
+    const dateTime = dateOnly[0] + " " + req.body.time + ":00";
+    // console.log(dateTime)
+    const toTimezone = req.body?.toTimezone;
+    const changeTime = [];
+    // for (let timeZone of toTimezone) {
+    //   const response = await axios.post(
+    //     "https://timeapi.io/api/Conversion/ConvertTimeZone",
+    //     {
+    //       fromTimeZone: req.body?.fromTimezone,
+    //       dateTime: dateTime,
+    //       toTimeZone: timeZone.timezone,
+    //       dstAmbiguity: "",
+    //     }
+    //   );
+    //   const timeObj = {
+    //     // name: timeZone.name,
+    //     timezone: timeZone?.timezone,
+    //     time: response?.data?.conversionResult?.time,
+    //     date: response?.data?.conversionResult?.date,
+    //     result: response?.data?.conversionResult,
+    //   };
+    //   console.log(timeObj)
+    //   changeTime.push(timeObj)
+    // }
+    const response = await axios.post(
+      "https://timeapi.io/api/Conversion/ConvertTimeZone",
+      {
+        fromTimeZone: req.body?.fromTimezone,
+        dateTime: dateTime,
+        toTimeZone: req.body?.toTimezone,
+        dstAmbiguity: "",
+      }
+    );
+    const obj = {
+      date: response?.data?.conversionResult.date,
+      time: response?.data?.conversionResult.time,
+    };
+    return res.status(200).json({
+      status: "Success",
+      data: obj,
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(400).json({
+      status: "Fail",
+    });
+  }
+});
+
+server.listen(PORT, "192.168.1.7");
---
+++ public/js/index.js
@@ -0,0 +1,80 @@
+let select = document.getElementById("select");
+async function getSearch() {
+  let search = document.getElementById("search").value;
+  let listInnerHTML = `<select id='list'>`;
+  listInnerHTML += "<option selected value=''> Select the city</option>";
+  const response = await fetch(
+    `http://192.168.1.7:4600/search/?search=${search}`
+  );
+  const data = await response.json();
+  // data.forEach(e => {
+  // });
+  for (let e of data) {
+    listInnerHTML += `<option value="${e.Offset} && ${e.Name}">${e.Name}</option>`;
+  }
+  listInnerHTML += `</option>`;
+  select.innerHTML = listInnerHTML;
+  let list = document.getElementById("list");
+  list.addEventListener("click", () => {
+    let cityName = list.value.split(" && ")[1];
+    let cityOffset = list.value.split(" && ")[0];
+    if (!cityName) return;
+
+    var originalDateTime = new Date().toUTCString();
+    var originalOffset = 0;
+    var targetOffset = Number(cityOffset) * 60;
+
+    var originalMoment = moment.utc(originalDateTime).utcOffset(originalOffset);
+
+    var targetMoment = originalMoment.clone().utcOffset(targetOffset);
+    var mainDiv = document.getElementById("mainDiv");
+    let cityHTML = "";
+    cityHTML += mainDiv.innerHTML;
+
+    cityHTML += `<div class="box">
+            <div style="display: flex;">
+              <div class="cityname">
+                <h1 style="margin: 0">${cityName}</h1>
+              </div>
+              <div class="time-date">
+                <input type="text" value='${targetMoment.format(
+                  "HH:mm"
+                )}' onchange='getValue(this.value, this.getAttribute("offset"), this.getAttribute("time"))' style="width: 60px; height: 25px; border-radius: 8px; outline-style: none; border: none; text-align: center;" time='${targetMoment.format()}' offset='${cityOffset}' id='inputTime'/>
+                <h3 style="display: flex; align-items: center; margin: 0; padding:0 10px;">
+                  ${targetMoment.format("DD-MM-YYYY")}
+                </h3>
+              </div>
+            </div>
+          </div>`;
+
+    mainDiv.innerHTML = cityHTML;
+    document.getElementById("search").value = "";
+    select.innerHTML = "";
+  });
+}
+
+async function getValue(getTime, offset, time) {
+  const convertTime = time.replace(/T\d{2}:\d{2}/, "T" + getTime);
+  if (moment(convertTime).format() == "Invalid date") {
+    console.log(getTime, "invalid date");
+    return;
+  }
+  const gmtTime = moment(convertTime).utc().format();
+
+  let inputTimeAll = document.querySelectorAll("#inputTime");
+  inputTimeAll.forEach((e) => {
+    let inputOffset = e.getAttribute("offset");
+    var originalDateTime = gmtTime;
+    var originalOffset = 0;
+    var targetOffset = Number(inputOffset) * 60;
+
+    var originalMoment = moment.utc(originalDateTime).utcOffset(originalOffset);
+
+    var targetMoment = originalMoment.clone().utcOffset(targetOffset);
+    e.value = targetMoment.format("HH:mm");
+    // e.setAttribute("value", targetMoment.format("HH:mm"));
+    const parentElement = e.parentNode || e.parentElement;
+    parentElement.querySelector("h3").innerText =
+      targetMoment.format("DD-MM-YYYY");
+  });
+}
